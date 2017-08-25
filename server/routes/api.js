ebc170c3dd5911d1a5c4907ba31cc8af548c5c92
Hacktiv8 <hacktiv8-5@Hacktiv8-5.local>
2017-08-25 15:23:49 +0700

    request azuer via client

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const apiController = require('../controllers/apiController')
 
 //get expression menerima req.query.url = lemparan dari get result
-router.get('/expression', apiController.getExpression)
+router.post('/expression', apiController.getExpression)
 router.get('/quotes/:mood', apiController.getQuotes)
 router.get('/images/:collectionId', apiController.getImage)
 
 //get result menerima req.query.url = url dari gambar yang telah di upload
-router.get('/get-result', apiController.getResult)
+router.post('/get-result', apiController.getResult)
+
+router.post('/upload-image', apiController.upload)
 
 module.exports = router;
