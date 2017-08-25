876ee5816d82c858848e2c791594b7c8a36e4dcf
Ahmad Fajar <shin.fajar@gmail.com>
2017-08-25 10:54:41 +0700

    tambah field mood

--- server/models/Quotes.js
+++ server/models/Quotes.js
@@ -5,7 +5,8 @@ const Schema   = mongoose.Schema;
 const QuotesSchema = mongoose.Schema({
   url    : String,
   quotes : String,
-  author : String
+  author : String,
+  mood   : String
 })
 
 const Quotes = mongoose.model('quotes', QuotesSchema);
